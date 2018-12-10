d85fbeb9ddeedd42f04d931430252435576a05cf
Nicholas Dechert <claymore2106@gmail.com>
2018-12-10 15:33:54 -0800

    Finished

--- String Reversal/StringReversal/StringReversal/Program.cs
+++ String Reversal/StringReversal/StringReversal/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 
-namespace StringReversal
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
+        Console.WriteLine("Enter a string to reverse:");
+        string ReverseThis = Console.ReadLine();
 
+        Console.WriteLine("You entered: {0}", ReverseThis);
+
+        string Reversed = "";
+        int Index = ReverseThis.Length - 1;
+        Console.WriteLine("Length is {0}", ReverseThis.Length);
+        
+        while (Index >= 0)
+        {
+            Reversed = Reversed + ReverseThis[Index];
+            Index--;
         }
+
+        Console.WriteLine("Reversed string: {0}", Reversed);
     }
 }
